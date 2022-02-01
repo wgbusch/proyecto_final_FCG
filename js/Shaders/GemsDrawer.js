33e05abe2c1cb548d9e00a77cedaee05dde7599f
Werner Busch <werner.busch@salesforce.com>
2022-02-01 01:07:07 -0300

    adding gems

--- js/Shaders/GemsDrawer.js
+++ js/Shaders/GemsDrawer.js
@@ -35,25 +35,15 @@ class GemsDrawer {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
 
         this.vertPos = this.createMeshOfGemsToDraw(indexesOfGemsToDraw);
-
+        // this.vertPos = [0,0,0, 1,1,1];
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertPos), gl.STATIC_DRAW);
 
         this.numTriangles = this.vertPos.length / 9;
         let colors = [];
         for (let i = 0; i < this.numTriangles; i++) {
-            if (i % 3 === 0) {
-                colors.push(1, 0, 0, 1);
-                colors.push(1, 0, 0, 1);
-                colors.push(1, 0, 0, 1);
-            } else if (i % 3 === 1) {
-                colors.push(0, 1, 0, 1);
-                colors.push(0, 1, 0, 1);
-                colors.push(0, 1, 0, 1);
-            } else if (i % 3 === 2) {
-                colors.push(0, 0, 1, 1);
-                colors.push(0, 0, 1, 1);
-                colors.push(0, 0, 1, 1);
-            }
+            colors.push(0.725, 0.239, 0.741, 1);
+            colors.push(0.725, 0.239, 0.741, 1);
+            colors.push(0.725, 0.239, 0.741, 1);
         }
 
         // setting up colour buffers
@@ -78,6 +68,8 @@ class GemsDrawer {
         // Dibujamos
         gl.useProgram(this.prog);
         gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles * 3);
+        // gl.drawElements(gl.LINES, this.numTriangles * 3, gl.UNSIGNED_SHORT, 0);
+
     }
 
     createMeshOfGemsToDraw(indexesOfGemsToDraw) {
@@ -91,18 +83,69 @@ class GemsDrawer {
             let zTranslation = (zIndex + 1 / 2) * (TOTAL_Z_LENGTH / numberOfZSquares) - TOTAL_Z_LENGTH / 2;
 
             let translatedGem =
-                [   -(TOTAL_X_LENGTH / (4 * numberOfXSquares)) + xTranslation,  0.001 + yTranslation,   0 - zTranslation,
-                    (TOTAL_X_LENGTH / (4 * numberOfXSquares)) + xTranslation,   0.001 + yTranslation,   0 - zTranslation,
-                    (TOTAL_X_LENGTH / (4 * numberOfXSquares)) + xTranslation,   0.1 + yTranslation,     0 - zTranslation,
-                    xTranslation,                                               0.001 + yTranslation,   (TOTAL_X_LENGTH / (4 * numberOfXSquares)) - zTranslation,
-                    xTranslation,                                               0.001 + yTranslation,   -(TOTAL_X_LENGTH / (4 * numberOfXSquares)) - zTranslation,
-                    xTranslation,                                               0.1 + yTranslation,     -(TOTAL_X_LENGTH / (4 * numberOfXSquares)) - zTranslation]
+                [-(TOTAL_X_LENGTH / (4 * numberOfXSquares)) + xTranslation, 0.001 + yTranslation, 0 - zTranslation,
+                    (TOTAL_X_LENGTH / (4 * numberOfXSquares)) + xTranslation, 0.001 + yTranslation, 0 - zTranslation,
+                    (TOTAL_X_LENGTH / (4 * numberOfXSquares)) + xTranslation, 0.1 + yTranslation, 0 - zTranslation,
+                    xTranslation, 0.001 + yTranslation, (TOTAL_X_LENGTH / (4 * numberOfXSquares)) - zTranslation,
+                    xTranslation, 0.001 + yTranslation, -(TOTAL_X_LENGTH / (4 * numberOfXSquares)) - zTranslation,
+                    xTranslation, 0.1 + yTranslation, -(TOTAL_X_LENGTH / (4 * numberOfXSquares)) - zTranslation]
+
 
             vertexes = vertexes.concat(translatedGem);
 
             result = indexesOfGemsToDraw.next();
         }
-        return vertexes;
+
+        let objToMeshConverter = new ObjToMeshConverter();
+
+        let obj = "v -0.000000 0.065948 0.018899\n" +
+            "v 0.014775 0.065948 0.011784\n" +
+            "v 0.018424 0.065948 -0.004204\n" +
+            "v 0.008200 0.065948 -0.017026\n" +
+            "v -0.008200 0.065948 -0.017026\n" +
+            "v -0.018424 0.065948 -0.004204\n" +
+            "v -0.014775 0.065948 0.011784\n" +
+            "v -0.000000 0.015793 0.000001\n" +
+            "v -0.000000 0.052440 0.033093\n" +
+            "v 0.025873 0.052440 0.020634\n" +
+            "v 0.032263 0.052440 -0.007363\n" +
+            "v 0.014358 0.052440 -0.029814\n" +
+            "v -0.014358 0.052440 -0.029814\n" +
+            "v -0.032263 0.052440 -0.007363\n" +
+            "v -0.025873 0.052440 0.020634\n" +
+            "s off\n" +
+            "f 9 8 10\n" +
+            "f 10 8 11\n" +
+            "f 11 8 12\n" +
+            "f 12 8 13\n" +
+            "f 13 8 14\n" +
+            "f 6 7 2\n" +
+            "f 14 8 15\n" +
+            "f 15 8 9\n" +
+            "f 1 10 2\n" +
+            "f 2 11 3\n" +
+            "f 3 12 4\n" +
+            "f 4 13 5\n" +
+            "f 6 13 14\n" +
+            "f 6 15 7\n" +
+            "f 7 9 1\n" +
+            "f 7 1 2\n" +
+            "f 2 3 6\n" +
+            "f 3 4 6\n" +
+            "f 4 5 6\n" +
+            "f 1 9 10\n" +
+            "f 2 10 11\n" +
+            "f 3 11 12\n" +
+            "f 4 12 13\n" +
+            "f 6 5 13\n" +
+            "f 6 14 15\n" +
+            "f 7 15 9\n"
+
+        objToMeshConverter.parse(obj);
+        let xx = objToMeshConverter.getVertexBuffers();
+
+        return xx.positionBuffer;
+        // return vertexes;
     }
 
     gemsVS = `
